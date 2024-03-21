5c0c0a3d670fa6d5e4555555010c8888ea07b24a
ChongMing <chongming_github@outlook.com>
2024-03-21 09:29:26 +0800

    创建项目

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # QR-Code_AccessControlSystem
+
 本科毕业设计-二维码门禁管理系统
 后端基于SpringBoot，前端基于SwiftUI（暂定）
---
+++ src/test/java/org/chongming/qrcode_accesscontrolsystem/QrCodeAccessControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chongming.qrcode_accesscontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeAccessControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
